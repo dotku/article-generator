6c8adafe7206711233a39c53f52591e22858f18a
dotku <weijingjaylin@gmail.com>
2020-09-25 11:33:39 +0000

    Deploying to gh-pages from  @ 8b41d8d4ed3b3574a2e3f0c67af7bebaf4d8efaa 🚀

---
+++ precache-manifest.715d70d142b5eb5491a679e3c072aa51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "577d3d9bec3372d41436e93dbdaacea2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "def7ef356eb5505fb0c7",
+    "url": "/static/css/main.aaaba402.chunk.css"
+  },
+  {
+    "revision": "99623bae3ae52444747f",
+    "url": "/static/js/2.a2dfd485.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/static/js/2.a2dfd485.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "def7ef356eb5505fb0c7",
+    "url": "/static/js/main.387c1b1c.chunk.js"
+  },
+  {
+    "revision": "6a2d3a6c227d6765b021",
+    "url": "/static/js/runtime-main.8ec67bd3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
